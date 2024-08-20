83a096c7b3cbcdd4172ef94f1f90e0b2e9f0c414
Vinicius Montes <viniciusmontes@outlook.com.br>
2024-08-20 18:03:51 -0300

    Modal datasheet in table now

--- frontweb/src/components/DataSheetModal/index.tsx
+++ frontweb/src/components/DataSheetModal/index.tsx
@@ -1,9 +1,7 @@
-import "./styles.css";
-
-import Modal from "react-modal";
-
-import { DataSheetDTO } from "../../models/data-sheet";
-import { formatPrice } from "../../utils/formatters";
+import './styles.css';
+import Modal from 'react-modal';
+import { DataSheetDTO } from '../../models/data-sheet';
+import { formatPrice } from '../../utils/formatters';
 
 type ProductModalProps = {
   isOpen: boolean;
@@ -26,21 +24,32 @@ export default function DataSheetModal({
       className="modal"
       overlayClassName="overlay"
       ariaHideApp={false}
+      shouldCloseOnOverlayClick={true}  // Fecha o modal ao clicar no plano de fundo
     >
       <h2 className="modal-header">Ficha Técnica - {productName}</h2>
-      <ul className="modal-list">
-        {dataSheet.materials.map((material) => (
-          <li key={material.materialId} className="modal-list-item">
-            Material ID: {material.materialId}, Nome do Material :{" "}
-            {material.materialName}, Custo do Material:{" "}
-            {formatPrice(material.materialCost)}, Quantidade:{" "}
-            {material.quantity}, Custo Usado: {formatPrice(material.costUsed)}
-          </li>
-        ))}
-      </ul>
-      <p className="total-cost">
-        Custo Total: {formatPrice(dataSheet.totalCost)}
-      </p>
+      <table className="modal-table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nome do Material</th>
+            <th>Custo do Material</th>
+            <th>Quantidade</th>
+            <th>Custo Usado</th>
+          </tr>
+        </thead>
+        <tbody>
+          {dataSheet.materials.map((material) => (
+            <tr key={material.materialId}>
+              <td>{material.materialId}</td>
+              <td>{material.materialName}</td>
+              <td>{formatPrice(material.materialCost)}</td>
+              <td>{material.quantity}</td>
+              <td>{formatPrice(material.costUsed)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <p className="total-cost">Custo Total: {formatPrice(dataSheet.totalCost)}</p>
       <button onClick={onRequestClose} className="close-button">
         Fechar
       </button>
